ecf4b974ae1aea4018812be4869ec7ee37fc71d9
6spotfrog <njhwheels@att.net>
2017-10-09 01:38:03 -0500

    window

--- Game Idea/Game/src/com/game/window/Window.java
+++ Game Idea/Game/src/com/game/window/Window.java
@@ -1,9 +1,24 @@
 package com.game.window;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 public class Window {
 
-	public Window(int w, int h, String GameTitle)
+	public Window(int w, int h, String GameTitle, Game game)
 	{
+		game.setPreferredSize(new Dimension(w,h));
+		game.setMaximumSize(new Dimension(w,h));
+		game.setMinimumSize(new Dimension(w,h));
+		
+		JFrame frame = new JFrame(GameTitle);
+		frame.add(game);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 		
 	}
 }
